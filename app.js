9eb48898ecb63214545404e5462a888acb935a67
Isaias Gutierrez <isaias.gutierrez48@gmail.comm>
2024-12-14 08:17:47 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,40 @@
+
+const list = document.getElementById("coinList");
+const button = document.getElementById('button');
+
+async function getData() {
+  try {
+    const request = await fetch(
+      `http://api.coinlayer.com/api/live?access_key=${apiKey}`
+    );
+    data = await request.json();
+
+    console.log(list);
+
+    Object.entries(data.rates).forEach(([coin,rate]) => {
+      const listItem = document.createElement("li");
+      const coinSpan = document.createElement('span');
+      const priceSpan = document.createElement('span');
+
+      //adds coin name to li
+      coinSpan.textContent = coin;
+
+      listItem.appendChild(coinSpan);
+
+      //adds coin price to li
+
+      priceSpan.textContent = rate;
+
+      listItem.appendChild(priceSpan);
+
+      //adds li to list
+
+      list.appendChild(listItem);
+      
+      button.style.display = 'none';
+
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
