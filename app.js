acf6fe071c5e813c6a9189c0b3d241053d99e338
Isaias Gutierrez <isaias.gutierrez48@gmail.comm>
2025-03-03 12:07:10 -0800

    button hover

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+const apiKey = "48b680aabbcbc42dc3926f6202c5083f";
 const list = document.getElementById("coinList");
 const button = document.getElementById('button');
 
@@ -12,25 +12,28 @@ async function getData() {
     console.log(list);
 
     Object.entries(data.rates).forEach(([coin,rate]) => {
+
+      //creates generics to be used for each entry
       const listItem = document.createElement("li");
       const coinSpan = document.createElement('span');
       const priceSpan = document.createElement('span');
 
-      //adds coin name to li
+      //creates a new span and sets content to coin name
       coinSpan.textContent = coin;
 
       listItem.appendChild(coinSpan);
 
-      //adds coin price to li
+      //creates a new span and sets content to rate of the coin
 
       priceSpan.textContent = rate;
 
       listItem.appendChild(priceSpan);
 
-      //adds li to list
-
+      //adds list item just made to list
       list.appendChild(listItem);
       
+
+      //hides 'click me' button after initial press
       button.style.display = 'none';
 
     });
